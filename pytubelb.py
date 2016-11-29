a07e354f848af48d4d8a36957e4c9bd28c666529
maxpoon <maxpanziyuan@gmail.com>
2016-11-29 11:13:26 +0800

    add function

--- pytubelb.py
+++ pytubelb.py
@@ -1,23 +1,32 @@
 import requests
 import threading
+import math
 from pytube import YouTube
 from bs4 import BeautifulSoup
+from os import listdir
 
-def download(url, resolution, path):
+def download(url, resolution, path, addOrder, n, numOfVideos):
 	try:
+		downloaded = listdir(path)
 		resolutions = ['720p', '480p', '360p', '240p', '144p']
 		yt = YouTube(url)
+		if yt.filename + '.mp4' in downloaded: return
 		if len(yt.filter('mp4', resolution=resolution)) == 0:
 			resolutions.remove(resolution)
 			for r in resolutions:
 				if len(yt.filter('mp4', resolution=r)) != 0:
 					resolution = r
 					break
+		if addOrder:
+			length = int(math.log(numOfVideos,10)) + 1
+			prefix = str(n)
+			prefix = "0"*(length - len(str(n))) + prefix +'. '
+			yt.set_filename(prefix + yt.filename)
 		video = yt.get('mp4', resolution)
-		print("downloading ", yt.filename)
+		print("downloading ", yt.filename, resolution)
 		video.download(path)
 	except:
-		download(url, resolution, path)
+		print("downloading ", yt.filename, "failed")
 
 #get user input
 resolutions = ['720p', '480p', '360p', '240p', '144p']
@@ -27,6 +36,8 @@ resolution = '480p' if resolution+'p' not in resolutions else resolution+'p'
 maxThreads = input("Maximum number of simultaneous downloads: ")
 maxThreads = int(maxThreads) if maxThreads.isdigit() else 5
 path = input('Path: ')
+addOrder = input("Add order number to the file name (Y/N): ")
+addOrder = True if addOrder=='Y' or addOrder=='y' else False
 if not maxThreads: maxThreads = 5
 if not path: path = './'
 
@@ -34,17 +45,21 @@ if not path: path = './'
 html =  requests.get(url).text
 soup = BeautifulSoup(html, 'html.parser')
 tags = soup.find_all('a')
-video_urls = set()
+video_urls = []
 for tag in tags:
 	video_url = tag.get('href', None)
 	if video_url and video_url.find('/watch?')==0: 
 		index = video_url.find('&list=')
 		video_url = video_url[:index]
-		video_urls.add('https://www.youtube.com'+video_url)
-video_urls = list(video_urls)
+		video_url = 'https://www.youtube.com'+video_url
+		if video_url not in video_urls:
+			video_urls.append(video_url)
 
+count = 1
+numOfVideos = len(video_urls)
 while len(video_urls):
 	if threading.active_count() >= maxThreads+1: continue
 	newURL = video_urls.pop(0)
-	newDownload = threading.Thread(target = download, args = (newURL, resolution, path))
+	newDownload = threading.Thread(target = download, args = (newURL, resolution, path,addOrder, count, numOfVideos))
 	newDownload.start()
+	count += 1
