7250a252f13cca0783a715e55cc3315aa8c6c7c1
Pan <zipan@paypal.com>
2017-02-12 23:03:07 +0800

    use thread-safe data structure

--- pytubelb.py
+++ pytubelb.py
@@ -1,12 +1,18 @@
+import time
 import requests
-import threading
+import concurrent.futures
+from queue import Queue
 import math
 from pytube import YouTube
 from bs4 import BeautifulSoup
 from os import listdir
+import multiprocessing
+cpus = multiprocessing.cpu_count()
 
-def download(url, resolution, path, addOrder, n, numOfVideos):
+def download(url, resolution, path, addOrder, numOfVideos, video_urls, completed):
 	try:
+		n = url[1]
+		url = url[0]
 		downloaded = listdir(path)
 		resolutions = ['720p', '480p', '360p', '240p', '144p']
 		yt = YouTube(url)
@@ -26,8 +32,11 @@ def download(url, resolution, path, addOrder, n, numOfVideos):
 		video = yt.get('mp4', resolution)
 		print("downloading ", yt.filename, resolution)
 		video.download(path)
+		completed.put(1)
 	except:
 		print("downloading ", yt.filename, "failed")
+		print("retry later")
+		video_urls.put((url, n))
 
 #get user input
 resolutions = ['720p', '480p', '360p', '240p', '144p']
@@ -35,7 +44,7 @@ url = input("YouTube Library URL: ")
 resolution = input("Resolution (144, 240, 360, 480, 720): ")
 resolution = '480p' if resolution+'p' not in resolutions else resolution+'p'
 maxThreads = input("Maximum number of simultaneous downloads: ")
-maxThreads = int(maxThreads) if maxThreads.isdigit() else 5
+maxThreads = int(maxThreads) if maxThreads.isdigit() else cpus
 path = input('Path: ')
 addOrder = input("Add order number to the file name (Y/N): ")
 addOrder = True if addOrder=='Y' or addOrder=='y' else False
@@ -46,22 +55,27 @@ if not path: path = './'
 html =  requests.get(url).text
 soup = BeautifulSoup(html, 'html.parser')
 tags = soup.find_all('a')
-video_urls = []
+video_urls = Queue()
+url_set = set()
+count = 1
 for tag in tags:
 	video_url = tag.get('href', None)
 	if video_url and video_url.find('/watch?')==0 and video_url.find('&index=')>0: 
 		index = video_url.find('&list=')
 		video_url = video_url[:index]
 		video_url = 'https://www.youtube.com'+video_url
-		if video_url not in video_urls:
-			video_urls.append(video_url)
+		if video_url not in url_set:
+			video_urls.put((video_url, count))
+			url_set.add(video_url)
+			count += 1
 
-count = 1
-numOfVideos = len(video_urls)
+numOfVideos = video_urls.qsize()
+completed = Queue()
+pool = concurrent.futures.ThreadPoolExecutor(max_workers=maxThreads)
 print("Started downloading ", numOfVideos, " videos")
-while len(video_urls):
-	if threading.active_count() >= maxThreads+1: continue
-	newURL = video_urls.pop(0)
-	newDownload = threading.Thread(target = download, args = (newURL, resolution, path,addOrder, count, numOfVideos))
-	newDownload.start()
-	count += 1
+start_time = time.time()
+while completed.qsize()<numOfVideos:
+	if video_urls.qsize()>0:
+		newURL = video_urls.get()
+		pool.submit(download, newURL, resolution, path, addOrder, numOfVideos, video_urls, completed)
+print("Download time: ", time.time()-start_time, "seconds")
